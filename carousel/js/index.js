03b102080434e6d1661b5ba350d241fa51d067fc
luodan <415391782@qq.com>
2015-04-21 15:27:33 +0800

    delete unuse code

--- carousel/js/index.js
+++ carousel/js/index.js
@@ -1,16 +1,12 @@
 $(document).ready(function() {
     $('#btn_video').hide();
     var videonum = videolist.length;
-    $(".pre").hover(function(){
-        $(".pre").attr("src","carousel/imgs/pre2.png");
+    $(".pre,.next").hover(function(){
+        $(this).attr("src","carousel/imgs/pre2.png");
     },function(){
-        $(".pre").attr("src","carousel/imgs/pre1.png");
+        $(this).attr("src","carousel/imgs/pre1.png");
     });
-    $(".next").hover(function(){
-        $(".next").attr("src","carousel/imgs/pre2.png");
-    },function(){
-        $(".next").attr("src","carousel/imgs/pre1.png");
-    });
+
     //根据视频个数生成图片预览列表
     for(var i = 0; i < videonum; i++){
         $("#div_img").append("<img id='img" + i + "' src='" + videolist[i].imgsrc + "'></img>");
@@ -22,11 +18,13 @@ $(document).ready(function() {
         $(".ol_nav .next").before("<li id=li"+i+"></li>");
     }
     var current = 0; //记录当前视频、视频预览
+    //为.cover定位，根据＃li0定位
     $(".cover").position({
         my: "center center",
         at: "center center",
         of: $('#li'+current)
     });
+    //当浏览器窗口大小变化时触发
     window.onresize= function(){
         $(".cover").position({
             my: "center center",
@@ -40,78 +38,48 @@ $(document).ready(function() {
             $("#sidebarWrapper").css("position","absolute");
         }
     }
-    //<div  class='cover' id=coverli"+i+">
     //当前视频预览图显示初始化
     switch_to(current);
-    //箭头导航事件添加
-    var cur_video = document.getElementsByTagName("#cur_video");
 
-    $(".ol_nav .cover li").tooltip();
-    $('[data-toggle="tooltip"]').tooltip();
+    //鼠标滑过li时，出现文字的标题
     $(".ol_nav li").hover(function(){
         var id = parseInt(event.target.id.substr(2,1));
-        $(this).attr({"data-toggle":"tooltip","data-placement":"top","title":videolist[id].head});
+        $(this).attr({"data-toggle":"tooltip","data-placement":"bottom","title":videolist[id].head});
     },function(){});
+    //$(".ol_nav li").tooltip();
+    $('[data-toggle="tooltip"]').tooltip();
 
-    $('#imgpre').click(function(){
-        $("#img"+current).css("visibility","visible");
-
+    //左右箭头事件
+    $('#imgpre, .pre').click(function(){
         switch_to(current-1);
-
-        //if(current - 1 < 0){
-        //    switch_to(4);
-        //}
     });
-    $('.pre').click(function(){
-        $("#img"+current).css("visibility","visible");
-
-        switch_to(current-1);
-
-        //if(current - 1 < 0){
-        //    switch_to(4);
-        //}
-    });
-    $('#imgnext').click(function(){
-        $("#img"+current).css("visibility","visible");
-
+    $('#imgnext, .next').click(function(){
         switch_to(current+1);
-
         if(current + 1 > videonum){
             switch_to(0);
         }
     });
-    $('.next').click(function(){
-        $("#img"+current).css("visibility","visible");
 
-        switch_to(current+1);
-
-        if(current + 1 > videonum){
-            switch_to(0);
-        }
-    });
-    //$("#coverli"+current).css({"visibility":"visible"});
     for(var i = 0; i < videonum; i++){
         //方块导航事件添加
         $('#li'+i).click(function(event){
             var id = parseInt(event.target.id.substr(2,1));
-            $("img").css("visibility","visible");
-            //$("#coverli"+id).css({"visibility":"visible"});
             switch_to(id);
         });
         //显示播放按钮事件
-        $('#img'+i).mouseover(function(event){
-            var id = parseInt(event.target.id.substr(3,1));
-            if(id == current){
-                $('#btn_video').show();
-            }
-        });
-        $('#img'+i).mouseout(function(event){
-            var id = parseInt(event.target.id.substr(3,1));
-            if(id == current){
-                $('#btn_video').hide();
-            }
-        });
-
+         $('#img'+i).hover(function(evevt){
+                 var id = parseInt(event.target.id.substr(3,1));
+                 if(id == current){
+                     $('#btn_video').show();
+                 }
+         },
+         function(event){
+             var id = parseInt(event.target.id.substr(3,1));
+             if(id == current){
+                 $('#btn_video').hide();
+             }
+         });
+        //点击当前图片可播放，两侧图片可滑动
         $('#img'+i).click(function(event) {
             var id = parseInt(event.target.id.substr(3,1));
             if(id != current) {
@@ -122,9 +90,8 @@ $(document).ready(function() {
         });
     }
 
-    $(".modal-videotitle").append(videolist[current].title);
     //播放按钮事件添加
-    $('#btn_video').mouseover (function(){
+    $('#btn_video').mouseover(function(){
         $('#btn_video').show();
     });
     $('#btn_video').click(function(){
@@ -135,36 +102,28 @@ $(document).ready(function() {
     $("#videoModal").on("hide.bs.modal", function(){
         $("iframe").attr({"src":""});
     });
-
+    //为modal上的iframe添加标题
+    $(".modal-videotitle").append(videolist[current].title);
 
     var show_cur_video = function(){
         $("iframe").attr({"src":videolist[current].videosrc});
-        $("iframe").css({"z-index":1});
-        //$(".innerimg img").css("opacity","0.5");
         $("#title"+current).css("color","#fff");
         $('#btn_video').hide();
         $("#videoModal").modal('show');
     };
 
     function switch_to(id){
-        //if(id < 0 || id >= videonum)
-        //    return;
         if(id<0)return;
         $('.innerimg').animate({left:'-='+(id-current)*510+'px'},500,function(){});
         $('.cover').animate({left:'+='+(id-current)*20+'px'},500,function(){});
-        $("#img"+current).css({"background-color":"#333","visibility":"visible"});
-        $("#article-title div").css("color","#888");
         current = id;
         $('.ol_nav li').css("background-color","#444");
         $('#li'+current).css("background-color","#5f5f5f");
 
-        //$('li').removeClass("hover-style");
-        //$('#li'+current).addClass("hover-style");
-
-        $("iframe").css("z-index",-1).attr("src","");
-        $(".innerimg img").css({"opacity":"0.5"});
-        $("#title"+current).css("color","#fff");
+        $(".innerimg img").css({"opacity":"0.5"});//切换时所有的img暗下来，只有当前图片亮
         $("#img"+current).css({"opacity":"1"});
+        $("#article-title div").css("color","#888");//切换时所有的标题暗下来，只有当前亮
+        $("#title"+current).css("color","#fff");
 
         $("#play-full-story div").css({"height": "1px","top": "-61px","opacity":"0"});
         $("#play-full-story #play-full"+current).css({"height": "26px","top": "-86px","opacity":"1"});
